327e87e49328ca2faf4026717cd30c389f99a85a
t.sasa <takuma.sasakubo@outlook.jp>
2025-03-29 16:36:28 +0900

    refactor: update keymaps.lua :recycle:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps
+local map = vim.api.nvim_set_keymap
+
 -- lazy
-vim.keymap.set("n", "<leader>m", "<cmd>Lazy<cr>", { desc = "Lazy", noremap = true })
+map("n", "<leader>m", "<cmd>Lazy<cr>", { desc = "Lazy", noremap = true })
 
-vim.keymap.set("n", "c", '"_c', { noremap = true })
-vim.keymap.set("n", "C", '"_C', { noremap = true })
-vim.keymap.set("n", "dd", '"_dd', { noremap = true })
+map("n", "c", '"_c', { noremap = true })
+map("n", "C", '"_C', { noremap = true })
+map("n", "dd", '"_dd', { noremap = true })
 
-vim.keymap.set("v", "c", '"_c', { noremap = true })
-vim.keymap.set("v", "C", '"_C', { noremap = true })
-vim.keymap.set("v", "dd", '"_dd', { noremap = true })
+map("v", "c", '"_c', { noremap = true })
+map("v", "C", '"_C', { noremap = true })
+map("v", "dd", '"_dd', { noremap = true })
 
--- Ctrl + d 現在日次を挿入
-vim.keymap.set("i", "<C-d>", "<C-R>=strftime('%Y-%m-%d %H:%M:%S')<CR>", { noremap = true })
+map("i", "<C-d>", '<C-R>=strftime("%Y-%m-%d %H:%M:%S")<CR>', { desc = "現在日時", noremap = true })
