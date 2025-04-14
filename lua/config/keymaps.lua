fc0f114debebd1831628ed02714d529e73183bd8
t.sasa-win-home <takuma.sasakubo@outlook.jp>
2025-04-14 12:21:03 +0900

    update: update keymaps.lua for removing x change without yanking :gear:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,14 +11,12 @@ map("n", "C", '"_C', { desc = "Change to end of line without yanking", noremap =
 map("n", "dd", '"_dd', { desc = "Delete line without yanking", noremap = true })
 map("n", "DD", '"_DD', { desc = "Delete line without yanking", noremap = true })
 map("n", "s", '"_s', { desc = "Change without yanking", noremap = true })
-map("n", "x", '"_x', { desc = "Change without yanking", noremap = true })
 
 map("v", "c", '"_c', { desc = "Change without yanking", noremap = true })
 map("v", "C", '"_C', { desc = "Change to end of line without yanking", noremap = true })
 map("v", "dd", '"_dd', { desc = "Delete line without yanking", noremap = true })
 map("v", "DD", '"_DD', { desc = "Delete line without yanking", noremap = true })
 map("v", "s", '"_s', { desc = "Change without yanking", noremap = true })
-map("v", "x", '"_x', { desc = "Change without yanking", noremap = true })
 map("v", "p", '"_dP', { desc = "Retains original clipboard content when pasting", noremap = true })
 
 map("i", "<C-d>", '<C-R>=strftime("%Y-%m-%d %H:%M:%S")<CR>', { desc = "現在日時", noremap = true })
