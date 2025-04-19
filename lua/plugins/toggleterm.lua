4f515a745ddc81d89af60b3532a0474fd604ba45
t.sasa <takuma.sasakubo@outlook.jp>
2025-04-20 01:19:03 +0900

    fix: :bug: fix setting direction in toggleterm.ua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -76,7 +76,7 @@ return {
       insert_mappings = true, -- whether or not the open mapping applies in insert mode
       terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
       persist_size = true,
-      direction = "horizontal" or "vertical" or "window" or "float",
+      direction = "horizontal",
       -- direction = "vertical",
       close_on_exit = true, -- close the terminal window when the process exits
       shell = function()
