36dee2e7d1d4644d94c40271ab882c52bece7c7b
t.sasa-win-home <takuma.sasakubo@outlook.jp>
2025-05-09 17:24:19 +0900

    update: :+1: add settings to enable extra plugins related to copilot.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,11 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- enable extras plugins
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
